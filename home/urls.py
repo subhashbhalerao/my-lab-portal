4c514dcd929cb73da5ae809c5399974685b20268
Subhash Bhalerao <subhash.bhalerao2008@gmail.com>
2023-09-23 17:29:30 +0530

    Created some views, templates and style

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('lab-rules/', views.lab_rules, name='lab-rules'),
+    path('tutorials/', views.tutorials, name='tutorials'),
+    path('experiment-lists/', views.exp_lists, name='exp-lists'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home/home.html')
+
+def lab_rules(request):
+    return render(request, 'home/lab-rules.html')
+
+def tutorials(request):
+    return render(request, 'home/tutorials.html')
+
+def exp_lists(request):
+    return render(request, 'home/experiment-lists.html')
